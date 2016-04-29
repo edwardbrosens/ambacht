fa2bef072d12b738f23b0f979db40d920d128f99
Edward Brosens <edwardbrosens@gmail.com>
2016-04-29 09:35:48 +0000

    changes

--- upload/admin/config.php
+++ upload/admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://dev.oc/admin/');
-define('HTTP_CATALOG', 'http://dev.oc/');
+define('HTTP_SERVER', 'http://54.171.196.6/admin/');
+define('HTTP_CATALOG', 'http://54.171.196.6/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://dev.oc/admin/');
-define('HTTPS_CATALOG', 'http://dev.oc/');
+define('HTTPS_SERVER', 'http://54.171.196.6/admin/');
+define('HTTPS_CATALOG', 'http://54.171.196.6/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/edwardbrosens/websites/ambacht/upload/admin/');
-define('DIR_SYSTEM', '/Users/edwardbrosens/websites/ambacht/upload/system/');
-define('DIR_IMAGE', '/Users/edwardbrosens/websites/ambacht/upload/image/');
-define('DIR_LANGUAGE', '/Users/edwardbrosens/websites/ambacht/upload/admin/language/');
-define('DIR_TEMPLATE', '/Users/edwardbrosens/websites/ambacht/upload/admin/view/template/');
-define('DIR_CONFIG', '/Users/edwardbrosens/websites/ambacht/upload/system/config/');
-define('DIR_CACHE', '/Users/edwardbrosens/websites/ambacht/upload/system/storage/cache/');
-define('DIR_DOWNLOAD', '/Users/edwardbrosens/websites/ambacht/upload/system/storage/download/');
-define('DIR_LOGS', '/Users/edwardbrosens/websites/ambacht/upload/system/storage/logs/');
-define('DIR_MODIFICATION', '/Users/edwardbrosens/websites/ambacht/upload/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/edwardbrosens/websites/ambacht/upload/system/storage/upload/');
-define('DIR_CATALOG', '/Users/edwardbrosens/websites/ambacht/upload/catalog/');
+define('DIR_APPLICATION', '/opt/bitnami/apache2/htdocs/ambacht/upload/admin/');
+define('DIR_SYSTEM', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/');
+define('DIR_IMAGE', '/opt/bitnami/apache2/htdocs/ambacht/upload/image/');
+define('DIR_LANGUAGE', '/opt/bitnami/apache2/htdocs/ambacht/upload/admin/language/');
+define('DIR_TEMPLATE', '/opt/bitnami/apache2/htdocs/ambacht/upload/admin/view/template/');
+define('DIR_CONFIG', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/config/');
+define('DIR_CACHE', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/storage/download/');
+define('DIR_LOGS', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/bitnami/apache2/htdocs/ambacht/upload/system/storage/upload/');
+define('DIR_CATALOG', '/opt/bitnami/apache2/htdocs/ambacht/upload/catalog/');
 
 // DB
 define('DB_DRIVER', 'mpdo');
 define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'oc');
+define('DB_PASSWORD', 'M2MOx7gazzu2');
+define('DB_DATABASE', 'wareambacht');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
